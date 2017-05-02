d05915cb0a6d8120d32e4a38d8392484210737e2
mikedacoder <michaeldryburgh@hotmail.com>
2017-05-01 22:20:57 -0600

    Corrected so About and Contact pages did not display categories.

--- content.php
+++ content.php
@@ -1,24 +1,5 @@
 <article class="single-content">
 			<h3><?php the_title(); ?></h3>
 			<?php the_post_thumbnail('landscape-large-thumbnail'); ?>
-			<?php the_content();?>
-			
-			<p class="post-info"><?php the_time('F j, Y g:i a'); ?> | by <a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>"><?php the_author(); ?></a> | Posted in
-
-			<?php
-
-			$categories = get_the_category();
-			$separator = ", ";
-			$output = '';
-
-			if ($categories) {
-				foreach ($categories as $category) {
-					$output .= '<a href="' . get_category_link($category->term_id) . '">' . $category->cat_name . '</a>'  . $separator;
-				}
-				echo trim($output, $separator);
-			}
-
-			?>
-
-			</p>
+			<?php the_content();?>			
 		</article>
